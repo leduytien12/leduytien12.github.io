17d04a6eb4614e79569ffbd1f9fd932fcd430876
leduytien12 <102808556+leduytien12@users.noreply.github.com>
2022-04-01 20:09:15 +0700

    Add files via upload

---
+++ LeDuyTienDz/js/config.js
@@ -0,0 +1,18 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "031012005 ",
+            owner: "LE DUY TIEN"
+        },
+      
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0352905897",
+            owner: "LE DUY TIEN",
+            fastTransfer: true
+        },
+       
+    ]
+};
---
+++ public/js/config.js
@@ -0,0 +1,60 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'LÊ DUY TIẾN',
+        'favicon' : 'avtweb.jpg',
+        'notice': 'LÊ DUY TIẾN ĐẸP TRAI SỐ 1 PHAN THIẾT HIHI'
+    },
+    'cover-image' : 'ItsukishimaGateJapan-day.gif',
+    'avatar-image' : 'avtweb.jpg',
+    'name' : 'Lê Duy Tiến', 
+    'is_tick' : true,
+    'skills' : [
+        'Đẹp Trai',
+        'Tài Giỏi',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'favicon.png',
+            'content' : 'LIÊN HỆ ADMIN',
+            'author' : 'Lê Duy Tiến',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'Lê Duy Tiến',
+            'value' : 'https://leduytienthanhtoan.ga/LeDuyTienDz/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@Thanh.Xuan.Toi.Va.Emm' ,
+            'value' : 'https://www.facebook.com/Thanh.Xuan.Toi.Va.Emm/',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Lê Duy Tiến',
+            'value' : 'https://zalo.me/0352905897',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : '035.2905.897',
+            'value' : 'tel:0352905897',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'bq.leduytien@gmail.com',
+            'value' : 'mailto:bq.leduytien@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
